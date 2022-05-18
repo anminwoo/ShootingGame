9bbd8124fe48d52438cea1316a483262c3c72792
anminwoo <minwoo5995@gmail.com>
2022-05-18 22:45:56 +0900

    Add Move()

--- Shooting Game/Assets/Scripts/CommonEnemy.cs
+++ Shooting Game/Assets/Scripts/CommonEnemy.cs
@@ -1,16 +1,32 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CommonEnemy : MonoBehaviour
+public class CommonEnemy : Entity
 {
     void Start()
     {
-        
+        hp            = maxHp;
+        maxHp         = 20;
+        baseDamage    = 5;
+        currentDamage = baseDamage;
+        baseSpeed     = 3f;
+        currentSpeed  = baseSpeed;
     }
-    
+
+    private void FixedUpdate()
+    {
+        Move();
+    }
+
     void Update()
     {
         
+    }
+
+    void Move()
+    {
+        transform.Translate(Vector3.left * currentSpeed * Time.deltaTime);
     }
 }
