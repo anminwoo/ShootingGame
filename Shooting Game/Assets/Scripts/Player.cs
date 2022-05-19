4e6c27b284189f890ab30af32a90fb6c4748dd65
anminwoo <minwoo5995@gmail.com>
2022-05-19 22:58:58 +0900

    Add Scripts

---
+++ Shooting Game/Assets/Scripts/Player.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public int   hp;
+    public int   maxHp;
+    public int   baseDamage;
+    public int   currentDamage;
+    public float baseSpeed;
+    public float currentSpeed;
+
+    private void Awake()
+    {
+        hp = 100;
+        maxHp = hp;
+        baseDamage = 10;
+        currentDamage = baseDamage;
+        baseSpeed = 10.0f;
+        currentSpeed = baseSpeed;
+    }
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Shooting Game/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,52 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private float _speed;
+
+    private BoxCollider2D collider;
+    private Player player;
+    
+    private void Awake()
+    {
+        collider = GetComponent<BoxCollider2D>();
+        
+        player = GameObject.FindWithTag("Player").GetComponent<Player>();
+        _speed = player.currentSpeed;
+    }
+
+    void Start()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
+    }
+
+    void Update()
+    {
+        
+    }
+
+    void Move()
+    {
+        float h = Input.GetAxisRaw("Horizontal");
+        float v = Input.GetAxisRaw("Vertical");
+
+        h = h * _speed * Time.deltaTime;
+        v = v * _speed * Time.deltaTime;
+        
+        transform.Translate(Vector3.right * h);
+        transform.Translate(Vector3.up * v);
+    }
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        Debug.Log(col.gameObject.name);
+    }
+}
