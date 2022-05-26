8ea5dc3601a1b8a5050abf112e53756b99fa126a
anminwoo <minwoo5995@gmail.com>
2022-05-26 20:25:03 +0900

    Revert "Add Bullet disappear"
    This reverts commit fb7e55398a70b6c6e0be96db3b4948ab9f7bdabd.

--- Shooting Game/Assets/Scripts/Player.cs
+++ Shooting Game/Assets/Scripts/Player.cs
@@ -6,15 +6,15 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     [Header("Player States")]
-    public int   currentHp; // 현재 체력
-    public int   maxHp; // 최대 체력
-    public int   minHp; // 최소 체력
+    public int   currentHp;
+    public int   maxHp;
+    public int   minHp;
     [Space]
-    public int   baseDamage; // 기본 공격력
-    public int   currentDamage; // 현재 공격력
+    public int   baseDamage;
+    public int   currentDamage;
     [Space]
-    public float baseSpeed; // 기본 이동속도
-    public float currentSpeed; // 현재 이동속도
+    public float baseSpeed;
+    public float currentSpeed;
 
     public GameObject bullet;
 
