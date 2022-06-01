ffe4cebb8e14d9ce3696b22c20a75ee693ab9edf
anminwoo <minwoo5995@gmail.com>
2022-06-01 17:19:18 +0900

    Add HealItem Scripts

---
+++ Shooting Game/Assets/Scripts/Items/HealItem.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.UIElements;
+using UnityEngine;
+
+public class HealItem : Item
+{
+    private int healPoint = 30;
+    void Start()
+    {
+        speed = 5f;
+    }
+
+    public override void Move()
+    {
+        transform.Translate(Vector3.left * speed * Time.deltaTime);
+    }
+
+    public override void UseItem()
+    {
+        Debug.Log($"회복 아이템");
+        GameManager.gameManager.HealHp(healPoint);
+    }
+}
