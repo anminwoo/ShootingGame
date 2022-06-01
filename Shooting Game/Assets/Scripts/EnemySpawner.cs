f6a32e5920c81254f7218dd06b3ca9f85a8431a7
anminwoo <minwoo5995@gmail.com>
2022-06-01 13:16:12 +0900

    Change Scripts Sequence

--- Shooting Game/Assets/Scripts/EnemySpawner.cs
+++ Shooting Game/Assets/Scripts/EnemySpawner.cs
@@ -20,7 +20,7 @@ public class EnemySpawner : MonoBehaviour
     {
         while (true)
         {
-            float randPosY = UnityEngine.Random.Range(-8f, 8.5f);
+            float randPosY = UnityEngine.Random.Range(-7f, 8.5f);
             Instantiate(enemies[0], new Vector3(transform.position.x, randPosY, transform.position.z), transform.rotation);
             yield return new WaitForSeconds(2f);
         }
