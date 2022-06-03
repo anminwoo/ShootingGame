7043e79dbb00cc6aaca620503c9d6c4059e90512
anminwoo <minwoo5995@gmail.com>
2022-06-03 09:26:58 +0900

    Fix item array

--- Shooting Game/Assets/Scripts/Manager/ItemManager.cs
+++ Shooting Game/Assets/Scripts/Manager/ItemManager.cs
@@ -8,16 +8,17 @@ public class ItemManager : MonoBehaviour
 {
     enum ItemNumber
     {
-        healItem = 0,
+        healItem    = 0,
         maxHpUpItem = 1,
-        shieldItem = 2,
+        shieldItem  = 2,
         speedUpItem = 3
     }
     
     public static ItemManager itemManager;
     
     [SerializeField] private Item[] items;
-    [Space]
+    [Space] 
+    [SerializeField] private int itemCount;
     [Header("Item Types")]
     [SerializeField] private Item healItem;
     [SerializeField] private Item maxHpUpItem;
@@ -26,11 +27,12 @@ public class ItemManager : MonoBehaviour
 
     private void Awake()
     {
+        itemCount = 4;
         healItem    = Resources.Load<Item>("Prefabs/Heal Item");
         maxHpUpItem = Resources.Load<Item>("Prefabs/MaxHpUp Item");
         shieldItem  = Resources.Load<Item>("Prefabs/Shield Item");
         speedUpItem = Resources.Load<Item>("Prefabs/SpeedUp Item");
-
+        items = new Item[itemCount];
         items[(int)ItemNumber.healItem]    = healItem;
         items[(int)ItemNumber.maxHpUpItem] = maxHpUpItem;
         items[(int)ItemNumber.shieldItem]  = shieldItem;
