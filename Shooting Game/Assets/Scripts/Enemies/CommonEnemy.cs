c750afd5389fb2bed2fbccfcbe24f4b800e3e7ca
anminwoo <minwoo5995@gmail.com>
2022-07-05 19:51:09 +0900

    Add CommonEnemy

---
+++ Shooting Game/Assets/Scripts/Enemies/CommonEnemy.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CommonEnemy : Enemy
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public override void EnemySetting()
+    {
+        maxHp         = 20;
+        currentHp     = maxHp;
+        baseDamage    = 5;
+        currentDamage = baseDamage;
+        baseSpeed     = 3.5f;
+        currentSpeed  = baseSpeed;
+        score         = 100;
+        exp           = 2;
+        dropChance    = 2;
+    }
+}
