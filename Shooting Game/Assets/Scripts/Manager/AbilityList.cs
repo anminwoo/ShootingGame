cd1ab8979f6c85e29141f92d378bd396607994cd
anminwoo <minwoo5995@gmail.com>
2022-07-05 19:52:15 +0900

    Add AbilityList

---
+++ Shooting Game/Assets/Scripts/Manager/AbilityList.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AbilityList : MonoBehaviour
+{
+    private List<T Ability, T AbilityImage> abilityList = new List<T Ability, T AbilityImage>();
+}
