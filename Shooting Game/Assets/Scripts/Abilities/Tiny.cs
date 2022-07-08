ed43c24d62b0bd43fcde96ebe4063b185e1c4f73
anminwoo <minwoo5995@gmail.com>
2022-07-08 09:54:23 +0900

    Amend Tiny

--- Shooting Game/Assets/Scripts/Abilities/Tiny.cs
+++ Shooting Game/Assets/Scripts/Abilities/Tiny.cs
@@ -4,10 +4,10 @@ using UnityEngine;
 
 public class Tiny : MonoBehaviour
 {
-    private float reduceSize = 0.90f;
+    private float amount = 0.90f;
     void Minimize()
     {
-        gameObject.transform.localScale = new Vector3(gameObject.transform.localScale.x * reduceSize, gameObject.transform.localScale.y * reduceSize);
+        gameObject.transform.localScale = new Vector3(gameObject.transform.localScale.x * amount, gameObject.transform.localScale.y * amount);
         Debug.Log($"Player가 작아졌습니다.");
     }
 }
