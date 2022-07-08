21fe412b1d305df755f234fcc6991f68878dfb78
anminwoo <minwoo5995@gmail.com>
2022-07-08 09:53:31 +0900

    Amend Huge

--- Shooting Game/Assets/Scripts/Abilities/Huge.cs
+++ Shooting Game/Assets/Scripts/Abilities/Huge.cs
@@ -4,10 +4,10 @@ using UnityEngine;
 
 public class Huge : MonoBehaviour
 {
-    private float growSize = 1.25f;
+    private float amount = 1.25f;
     public void Expand()
     {
-        gameObject.transform.localScale = new Vector3(gameObject.transform.localScale.x * growSize, gameObject.transform.localScale.y * growSize);
+        gameObject.transform.localScale = new Vector3(gameObject.transform.localScale.x * amount, gameObject.transform.localScale.y * amount);
         Debug.Log($"Player가 커졌습니다.");
     }
 }
