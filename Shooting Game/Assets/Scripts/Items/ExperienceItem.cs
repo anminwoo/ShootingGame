4cb13d8ce79f6c72c96609b89ae5583a38203692
anminwoo <minwoo5995@gmail.com>
2022-07-09 15:00:13 +0900

    Add ExpItem script

---
+++ Shooting Game/Assets/Scripts/Items/ExperienceItem.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class ExperienceItem : Item
+{
+    private int expAmount = 5;
+    private void Start()
+    {
+        speed = 6.0f;
+    }
+    
+    private void Update()
+    {
+        Move();
+    }
+
+    public override void Move()
+    {
+        transform.Translate(Vector3.left * speed * Time.deltaTime);
+    }
+
+    public override void UseItem()
+    {
+        GameManager.gameManager.AddExp(expAmount);
+    }
+}
